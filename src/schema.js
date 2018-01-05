076dc49813205ab1c433603eb996419097f31f2f
Dave Gaeddert <dave.gaeddert@gmail.com>
2018-01-05 13:53:36 -0600

    <Dependencies> instead of <DependenciesSchema>

--- src/schema.js
+++ src/schema.js
@@ -1,6 +1,6 @@
 export const outputSchema = schema => {
-  const begin = '<DependenciesSchema>'
-  const end = '</DependenciesSchema>'
+  const begin = '<Dependencies>'
+  const end = '</Dependencies>'
   // schema.version = '2.0.0'
   console.log(begin + JSON.stringify(schema) + end)
   // console.log(JSON.stringify(schema))
